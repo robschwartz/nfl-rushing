3e8a2d0baa8e055f571fe5ae8550c4acbb6cef74
Rob Schwartz <rob.schwartz@vibes.com>
2020-05-15 11:39:47 -0400

    ADD: added caching for better performance

--- app/controllers/nfl_controller.rb
+++ app/controllers/nfl_controller.rb
@@ -1,9 +1,9 @@
 class NflController < ApplicationController
-  include Pagy::Backend
+  caches_action :show_stats
 
   def show_stats
     stat = params[:stat]
-    @pagy, @data = pagy_array(NflApiService.pull_stat_data(stat), items: 20)
+    @data = NflApiService.pull_stat_data(stat)
     @headers = @data[0].keys
     @sortable_cols = ['Yds', 'Lng', 'TD']
     @searchable_cols = ['Player']

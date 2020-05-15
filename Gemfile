deea7c87bd25ef9b136dd88a29e409dfa16f386a
Rob Schwartz <rob.schwartz@vibes.com>
2020-05-15 14:44:29 -0400

    TRY: attempting to scale with caching

--- Gemfile
+++ Gemfile
@@ -23,8 +23,8 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
-gem 'actionpack-action_caching'
-
+# For caching data 
+gem 'activesupport-core-ext', '~> 4.0', '>= 4.0.0.3'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/nfl_controller.rb
+++ app/controllers/nfl_controller.rb
@@ -1,11 +1,46 @@
 class NflController < ApplicationController
-  caches_action :show_stats
+  # caches_action :show_stats
+    before_action :set_cache
+    before_action :set_stat
+    before_action :set_data
 
   def show_stats
-    stat = params[:stat]
-    @data = NflApiService.pull_stat_data(stat)
+    puts 'show stats-----'
+
     @headers = @data[0].keys
     @sortable_cols = ['Yds', 'Lng', 'TD']
     @searchable_cols = ['Player']
+  end
+
+  private
+  
+  def all_cache
+    puts 'Set cached data'
+    @cache.write('stat_data', stat_data(@stat))
+    puts @cache.exist?('stat_data')
+  end
+
+  def stat_data(stat)
+    puts 'Pull stat data'
+    NflApiService.pull_stat_data(stat)
+  end
+
+  def set_stat
+    puts 'Set stat'
+    @stat = params[:stat]
+  end
+
+  def set_data
+    puts 'Set data'
+    puts @cache.exist?('stat_data')
+    if !@cache.exist?('stat_data')
+      all_cache
+    end
+
+    @data = @cache.read('stat_data')
+  end
+
+  def set_cache
+    @cache ||= ActiveSupport::Cache::MemoryStore.new
   end
 end

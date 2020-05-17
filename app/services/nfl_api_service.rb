6e505f416e1386c17763c20623c1aa4da82dd87c
Rob Schwartz <rob.schwartz@vibes.com>
2020-05-17 14:11:40 -0400

    UPDATE: Persist Search

--- app/services/nfl_api_service.rb
+++ app/services/nfl_api_service.rb
@@ -7,14 +7,16 @@ module NflApiService
   def pull_stat_data(stat, sort, sort_col, search)
     file = File.read("lib/#{stat}.json")
     stat_hash = JSON.parse(file)
+   
     if !search.blank?
       stat_hash = stat_hash.select { |x| x['Player'].downcase.include?(search.downcase)}
-      puts [stat_hash]
-    else
-      stat_hash.sort_by!{ |k| k[sort_col].is_a?(String)? k[sort_col].delete(",").to_i : k[sort_col] }
-
-      stat_hash.reverse! if sort == "acs"
+      stat_hash
     end
+   
+    stat_hash.sort_by!{ |k| k[sort_col].is_a?(String)? k[sort_col].delete(",").to_i : k[sort_col] }
+
+    stat_hash.reverse! if sort == "acs"
+    
     stat_hash
   end
 end

1e844db0a46557e8cef15d2367dafca622db16b1
Rob Schwartz <rob.schwartz@vibes.com>
2020-05-17 10:45:57 -0400

    UPDATE: Added bootstrap to pagination

--- app/controllers/nfl_controller.rb
+++ app/controllers/nfl_controller.rb
@@ -6,10 +6,8 @@ class NflController < ApplicationController
     @sort = params[:sort]
     @sort_col = params[:sort_col]
     @page = params[:page]
-    puts Time.now
     @data = NflApiService.pull_stat_data(@stat, @sort, @sort_col, @search)
     @pagy, @data = pagy_array(@data)
-    puts Time.now
     @headers = @data[0].keys
     @sortable_cols = ['Yds', 'Lng', 'TD']
     @searchable_cols = ['Player']

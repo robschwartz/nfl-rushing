1355e01170e4b9172b2f14dbec0b735dc53a3886
Rob Schwartz <rob.schwartz@vibes.com>
2020-05-17 20:13:21 -0400

    ADD: Added csv download

--- app/controllers/nfl_controller.rb
+++ app/controllers/nfl_controller.rb
@@ -10,6 +10,15 @@ class NflController < ApplicationController
     @pagy, @data = pagy_array(@data)
     @headers = @data[0].keys 
     @sortable_cols = ['Yds', 'Lng', 'TD']
-    @searchable_cols = ['Player']    
+    @searchable_cols = ['Player']       
+  end
+
+  def download_csv
+    @stat = params[:stat]
+    @sort = params[:sort]
+    @sort_col = params[:sort_col]
+    @search = params[:search]
+    @data = NflApiService.pull_stat_data(@stat, @sort, @sort_col, @search)
+    send_data @data, filename: "users-#{Date.today}.csv", disposition: 'attachment'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'nfl/:stat', to:"nfl#show_stats", as: 'show_stats'
+  get 'nfl/:stat', to:'nfl#show_stats', as: 'show_stats'
+  post '/download-data', to:'nfl#download_csv', as: 'download_csv'
 end
